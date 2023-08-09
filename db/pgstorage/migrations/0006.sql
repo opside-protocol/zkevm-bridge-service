0615773fd49797376b83cbe7564b2b9ab2e379ea
Surou <cppfuns@gmail.com>
2023-08-09 18:31:03 +0800

    fix# code

--- db/pgstorage/migrations/0006.sql
+++ db/pgstorage/migrations/0006.sql
@@ -1,2 +1,4 @@
 -- +migrate Down
 ALTER TABLE sync.monitored_txs ADD COLUMN is_l1 BOOLEAN DEFAULT FALSE;
+alter table sync.monitored_txs drop constraint monitored_txs_pkey;
+alter table sync.monitored_txs add primary key (id, is_l1);
